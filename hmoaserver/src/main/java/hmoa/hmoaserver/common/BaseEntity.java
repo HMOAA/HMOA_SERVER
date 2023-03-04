ff3884f04223cb0dfdf8020ed3c66836232079be
jjonghyuni <whdgusdl8792@gmail.com>
2023-03-05 00:20:53 +0900

    [Feat] Member 도메인 생성

---
+++ hmoaserver/src/main/java/hmoa/hmoaserver/common/BaseEntity.java
@@ -0,0 +1,28 @@
+package hmoa.hmoaserver.common;
+
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public void modifyUpdatedAt(LocalDateTime now) {
+        this.updatedAt = now;
+    }
+}
---
+++ hmoaserver/src/main/java/hmoa/hmoaserver/member/domain/Member.java
@@ -0,0 +1,75 @@
+package hmoa.hmoaserver.member.domain;
+
+
+
+import hmoa.hmoaserver.common.BaseEntity;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity implements UserDetails {
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String name;
+    private Integer age;
+    private String sex;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles = new HashSet<>();
+    @Builder
+    public Member(String email,String password, String name, Integer age, String sex){
+        this.email=email;
+        this.password=password;
+        this.name=name;
+        this.age=age;
+        this.sex=sex;
+        this.roles.add("USER");
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return getId().toString();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
