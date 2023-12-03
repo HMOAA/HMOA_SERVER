26d69a669b0e4435bbf7e9a52aafe01f5b8be2de
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-12-04 00:20:14 +0900

    [Fix] 내가 쓴 커뮤니티 댓글 조회에서 댓글 id -> 게시글 id 반환하도록 수정

--- hmoaserver/src/main/java/hmoa/hmoaserver/community/dto/CommunityCommentByMemberResponseDto.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/community/dto/CommunityCommentByMemberResponseDto.java
@@ -2,20 +2,19 @@ package hmoa.hmoaserver.community.dto;
 
 import hmoa.hmoaserver.common.DateUtils;
 import hmoa.hmoaserver.community.domain.CommunityComment;
-import hmoa.hmoaserver.member.domain.Member;
 import lombok.Data;
 
 @Data
-public class CommunityCommentDefaultResponseDto {
-    private Long commentId;
+public class CommunityCommentByMemberResponseDto {
+    private Long communityId;
     private String content;
     private String author;
     private String profileImg;
     private String time;
     private boolean writed;
 
-    public CommunityCommentDefaultResponseDto(CommunityComment comment, boolean writed){
-        this.commentId = comment.getId();
+    public CommunityCommentByMemberResponseDto(CommunityComment comment, boolean writed){
+        this.communityId = comment.getCommunity().getId();
         this.content = comment.getContent();
         this.author = comment.getMember().getNickname();
         this.profileImg = comment.getMember().getMemberPhoto().getPhotoUrl();
