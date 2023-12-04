4da35d334b7efeb4068cbccc1d79b1a305895b4f
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-12-04 15:55:29 +0900

    [Feat] 메인 페이지 두 번째 호출 API 구현

--- hmoaserver/src/main/java/hmoa/hmoaserver/homemenu/controller/MainPageController.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/homemenu/controller/MainPageController.java
@@ -8,6 +8,7 @@ import hmoa.hmoaserver.homemenu.service.HomeMenuService;
 import hmoa.hmoaserver.perfume.domain.Perfume;
 import hmoa.hmoaserver.homemenu.dto.HomeMenuPerfumeResponseDto;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -26,19 +27,47 @@ import java.util.stream.Collectors;
 @RequestMapping("/main")
 @Slf4j
 public class MainPageController {
-    private static final long FIRST_HOME_ID = 1L;
-    private static final long SECOND_HOME_ID = 2L;
-    private static final long THIRD_HOME_ID = 3L;
+    private static final long BRAND_HOME_ID = 1L;
+    private static final long FIRST_HOME_ID = 2L;
+    private static final long SECOND_HOME_ID = 3L;
+    private static final long THIRD_HOME_ID = 4L;
 
     private final HomeMenuService homeMenuService;
 
+    @ApiOperation("메인 페이지 첫 번째")
     @GetMapping("/first")
     public ResponseEntity<HomeMenuFirstResponseDto> getHomePerfumeList() {
-        HomeMenu homeMenu = homeMenuService.findHomeMenuById(FIRST_HOME_ID);
+        HomeMenu homeMenu = homeMenuService.findHomeMenuById(BRAND_HOME_ID);
         List<Perfume> perfumes = homeMenuService.findPerfumesByHomeMenu(homeMenu);
         Page<Perfume> perfumePage = new PageUtil<Perfume>().convertListToPage(perfumes, 0, 5);
         List<HomeMenuPerfumeResponseDto> perfumeResponseDtos = perfumePage.stream().map(HomeMenuPerfumeResponseDto::new).collect(Collectors.toList());
         HomeMenuFirstResponseDto result = new HomeMenuFirstResponseDto(new HomeMenuDefaultResponseDto(homeMenu.getTitle(), perfumeResponseDtos));
         return ResponseEntity.ok(result);
+    }
+
+    @ApiOperation("메인 페이지 두 번째")
+    @GetMapping("/second")
+    public ResponseEntity<List<HomeMenuDefaultResponseDto>> getSecondHomePerfumeList() {
+        HomeMenu firstMenu = homeMenuService.findHomeMenuById(FIRST_HOME_ID);
+        HomeMenu secondMenu = homeMenuService.findHomeMenuById(SECOND_HOME_ID);
+        HomeMenu thirdMenu = homeMenuService.findHomeMenuById(THIRD_HOME_ID);
+
+        List<HomeMenuPerfumeResponseDto> firstPerfumesInfo = getPerfumeResponseDto(firstMenu.getPerfumeList());
+        List<HomeMenuPerfumeResponseDto> secondPerfumesInfo = getPerfumeResponseDto(secondMenu.getPerfumeList());
+        List<HomeMenuPerfumeResponseDto> thirdPefumesInfo = getPerfumeResponseDto(thirdMenu.getPerfumeList());
+
+        HomeMenuDefaultResponseDto firstDto = new HomeMenuDefaultResponseDto(firstMenu.getTitle(), firstPerfumesInfo);
+        HomeMenuDefaultResponseDto secondDto = new HomeMenuDefaultResponseDto(secondMenu.getTitle(), secondPerfumesInfo);
+        HomeMenuDefaultResponseDto thirdDto = new HomeMenuDefaultResponseDto(thirdMenu.getTitle(), thirdPefumesInfo);
+
+        return ResponseEntity.ok(List.of(firstDto, secondDto, thirdDto));
+    }
+
+    private static List<HomeMenuPerfumeResponseDto> getPerfumeResponseDto(List<Perfume> perfumes) {
+        return convertListToPage(perfumes).stream().map(HomeMenuPerfumeResponseDto::new).collect(Collectors.toList());
+    }
+
+    private static Page<Perfume> convertListToPage(List<Perfume> perfumes) {
+        return new PageUtil<Perfume>().convertListToPage(perfumes, 0, 5);
     }
 }
