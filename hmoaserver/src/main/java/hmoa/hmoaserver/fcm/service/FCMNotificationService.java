18ba5baa6a796db1206de40278189fe23c7bdcbc
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2024-06-02 17:02:31 +0900

    [Feat] 푸쉬 알림 서비스 전체적으로 수정 - interface 팩토리 패턴으로 변경하며 변경사항 적용 - makeMessage() 각 타입에 맞는 메세지 만드는 함수 구현

--- hmoaserver/src/main/java/hmoa/hmoaserver/fcm/service/FCMNotificationService.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/fcm/service/FCMNotificationService.java
@@ -9,8 +9,12 @@ import hmoa.hmoaserver.fcm.domain.AlarmCategory;
 import hmoa.hmoaserver.fcm.domain.PushAlarm;
 import hmoa.hmoaserver.fcm.dto.FCMNotificationRequestDto;
 import hmoa.hmoaserver.fcm.repository.PushAlarmRepository;
+import hmoa.hmoaserver.fcm.service.constant.NotificationConstants;
+import hmoa.hmoaserver.fcm.service.constant.NotificationMessage;
+import hmoa.hmoaserver.fcm.service.constant.NotificationMessageFactory;
 import hmoa.hmoaserver.member.domain.Member;
 import hmoa.hmoaserver.member.repository.MemberRepository;
+import hmoa.hmoaserver.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -29,118 +33,149 @@ import static hmoa.hmoaserver.fcm.service.constant.NotificationConstants.*;
 @Service
 public class FCMNotificationService {
     private final FirebaseMessaging firebaseMessaging;
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
     private final PushAlarmRepository pushAlarmRepository;
 
-    public String sendNotification(FCMNotificationRequestDto requestDto) {
-        Optional<Member> member = memberRepository.findById(requestDto.getReceiverId());
+    public void sendNotification(FCMNotificationRequestDto requestDto) {
+        Optional<Member> member = memberService.findById(requestDto.getReceiverId());
 
+        if (!isValidNotification(requestDto, member)) {
+            return;
+        }
+
+        NotificationMessage notificationMessage = NotificationMessageFactory.getMessage(requestDto.getType());
+        
+
+
+
+    }
+
+    private static boolean isValidNotification(FCMNotificationRequestDto requestDto, Optional<Member> member) {
         if (isSameMember(requestDto.getReceiverId(), requestDto.getSenderId())) {
-            return SEND_NOT_REQUIRED;
+            return false;
         }
 
         if (member.isEmpty()) {
-            return NOT_FOUND_MEMBER;
+            return false;
         }
 
         if (member.get().getFirebaseToken() == null) {
-            return NOT_FOUND_TOKEN;
+            return false;
         }
 
-        String successControl = "";
-        String message = "";
-        AlarmCategory category = null;
-
-        if (requestDto.getType() == PERFUME_COMMENT_LIKE) {
-            successControl = sendCommentLike(member.get(), requestDto.getSender());
-            message = requestDto.getSender() + LIKE_COMMENT_ALARM_MESSAGE;
-            category = perfume_comment_like;
-        } else if (requestDto.getType() == COMMUNITY_COMMENT_LIKE) {
-            successControl = sendCommentLike(member.get(), requestDto.getSender());
-            message = requestDto.getSender() + LIKE_COMMENT_ALARM_MESSAGE;
-            category = community_comment;
-        } else if (requestDto.getType() == COMMUNITY_LIKE) {
-            successControl = sendCommunityLike(member.get(), requestDto.getSender());
-            message = requestDto.getSender() + LIKE_COMMUNITY_ALARM_MESSAGE;
-            category = community_like;
-        } else if (requestDto.getType() == COMMUNITY_COMMENT) {
-            successControl = sendAddComment(member.get(), requestDto.getSender());
-            message = requestDto.getSender() + ADD_COMMENT_ALARM_MESSAGE;
-            category = community_comment_like;
-        }
-
-//        savePushAlarm(message, category, member.get(), successControl, requestDto.getTargetId());
-        log.info("{}", successControl);
-        return successControl;
+        return true;
     }
 
 
-//    @Transactional
-//    public void savePushAlarm(String message, AlarmCategory category, Member member, String success, Long targetId) {
-//        if (success.equals(SUCCESS_SEND)) {
-//            PushAlarm alarm = PushAlarm.builder()
-//                    .alarmCategory(category)
-//                    .content(message)
-//                    .member(member)
-//                    .parentId(targetId)
-//                    .build();
+//    public String sendNotification(FCMNotificationRequestDto requestDto) {
+//        Optional<Member> member = memberRepository.findById(requestDto.getReceiverId());
 //
-//            pushAlarmRepository.save(alarm);
+//        if (isSameMember(requestDto.getReceiverId(), requestDto.getSenderId())) {
+//            return SEND_NOT_REQUIRED;
 //        }
+//
+//        if (member.isEmpty()) {
+//            return NOT_FOUND_MEMBER;
+//        }
+//
+//        if (member.get().getFirebaseToken() == null) {
+//            return NOT_FOUND_TOKEN;
+//        }
+//
+//        String successControl = "";
+//        String message = "";
+//        AlarmCategory category = null;
+//
+//        if (requestDto.getType() == PERFUME_COMMENT_LIKE) {
+//            successControl = sendCommentLike(member.get(), requestDto.getSender());
+//            message = requestDto.getSender() + LIKE_COMMENT_ALARM_MESSAGE;
+//            category = perfume_comment_like;
+//        } else if (requestDto.getType() == COMMUNITY_COMMENT_LIKE) {
+//            successControl = sendCommentLike(member.get(), requestDto.getSender());
+//            message = requestDto.getSender() + LIKE_COMMENT_ALARM_MESSAGE;
+//            category = community_comment;
+//        } else if (requestDto.getType() == COMMUNITY_LIKE) {
+//            successControl = sendCommunityLike(member.get(), requestDto.getSender());
+//            message = requestDto.getSender() + LIKE_COMMUNITY_ALARM_MESSAGE;
+//            category = community_like;
+//        } else if (requestDto.getType() == COMMUNITY_COMMENT) {
+//            successControl = sendAddComment(member.get(), requestDto.getSender());
+//            message = requestDto.getSender() + ADD_COMMENT_ALARM_MESSAGE;
+//            category = community_comment_like;
+//        }
+//
+////        savePushAlarm(message, category, member.get(), successControl, requestDto.getTargetId());
+//        log.info("{}", successControl);
+//        return successControl;
 //    }
-
-    @Transactional(readOnly = true)
-    public Page<PushAlarm> findPushAlarms(Member member) {
-        return pushAlarmRepository.findAllByMemberOrderByCreatedAtDesc(member, PageRequest.of(PageSize.ZERO_PAGE.getSize(), PageSize.TEN_SIZE.getSize()));
-    }
-
-    @Transactional
-    public void readPushAlarm(PushAlarm pushAlarm) {
-        pushAlarm.read();
-    }
-
-    private String sendCommunityCommentLike(Member member, String sender, Long targetId) {
-        Message message = makeMessage(member, LIKE_ALARM_TITLE, sender + LIKE_COMMENT_ALARM_MESSAGE, makeUri(community_comment_like.name(), targetId));
-        return send(message);
-    }
-
-    private String sendCommunityLike(Member member, String sender, Long targetId) {
-        Message message = makeMessage(member, LIKE_ALARM_TITLE, sender + LIKE_COMMUNITY_ALARM_MESSAGE, makeUri(community_comment_like.name(), targetId));
-        return send(message);
-    }
-
-    private String sendAddComment(Member member, String sender, Long targetId) {
-        Message message = makeMessage(member, ADD_COMMENT_ALARM_TITLE, sender + ADD_COMMENT_ALARM_MESSAGE);
-        return send(message);
-    }
-
-    private static String makeUri(String category, long id) {
-        return String.format(URI_MAPPING, category, id);
-    }
-
-    private static Message makeMessage(Member member, String title, String body, String uri) {
-        Notification notification = Notification.builder()
-                .setTitle(title)
-                .setBody(body)
-                .build();
-
-        return Message.builder()
-                .setToken(member.getFirebaseToken())
-                .setNotification(notification)
-                .putData(DEEPLINK_TITLE, uri)
-                .build();
-    }
-
-    private String send(Message message) {
-        try {
-            firebaseMessaging.send(message);
-            return SUCCESS_SEND;
-        } catch (FirebaseMessagingException e) {
-            e.printStackTrace();
-        }
-        return FAILE_SEND;
-    }
-
+//
+//
+////    @Transactional
+////    public void savePushAlarm(String message, AlarmCategory category, Member member, String success, Long targetId) {
+////        if (success.equals(SUCCESS_SEND)) {
+////            PushAlarm alarm = PushAlarm.builder()
+////                    .alarmCategory(category)
+////                    .content(message)
+////                    .member(member)
+////                    .parentId(targetId)
+////                    .build();
+////
+////            pushAlarmRepository.save(alarm);
+////        }
+////    }
+//
+//    @Transactional(readOnly = true)
+//    public Page<PushAlarm> findPushAlarms(Member member) {
+//        return pushAlarmRepository.findAllByMemberOrderByCreatedAtDesc(member, PageRequest.of(PageSize.ZERO_PAGE.getSize(), PageSize.TEN_SIZE.getSize()));
+//    }
+//
+//    @Transactional
+//    public void readPushAlarm(PushAlarm pushAlarm) {
+//        pushAlarm.read();
+//    }
+//
+//    private String sendCommunityCommentLike(Member member, String sender, Long targetId) {
+//        Message message = makeMessage(member, LIKE_ALARM_TITLE, sender + LIKE_COMMENT_ALARM_MESSAGE, makeUri(community_comment_like.name(), targetId));
+//        return send(message);
+//    }
+//
+//    private String sendCommunityLike(Member member, String sender, Long targetId) {
+//        Message message = makeMessage(member, LIKE_ALARM_TITLE, sender + LIKE_COMMUNITY_ALARM_MESSAGE, makeUri(community_comment_like.name(), targetId));
+//        return send(message);
+//    }
+//
+//    private String sendAddComment(Member member, String sender, Long targetId) {
+//        Message message = makeMessage(member, ADD_COMMENT_ALARM_TITLE, sender + ADD_COMMENT_ALARM_MESSAGE);
+//        return send(message);
+//    }
+//
+//    private static String makeUri(String category, long id) {
+//        return String.format(URI_MAPPING, category, id);
+//    }
+//
+//    private static Message makeMessage(Member member, String title, String body, String uri) {
+//        Notification notification = Notification.builder()
+//                .setTitle(title)
+//                .setBody(body)
+//                .build();
+//
+//        return Message.builder()
+//                .setToken(member.getFirebaseToken())
+//                .setNotification(notification)
+//                .putData(DEEPLINK_TITLE, uri)
+//                .build();
+//    }
+//
+//    private String send(Message message) {
+//        try {
+//            firebaseMessaging.send(message);
+//            return SUCCESS_SEND;
+//        } catch (FirebaseMessagingException e) {
+//            e.printStackTrace();
+//        }
+//        return FAILE_SEND;
+//    }
+//
     private static boolean isSameMember(Long receiverId, Long senderId) {
         return receiverId.equals(senderId);
     }
