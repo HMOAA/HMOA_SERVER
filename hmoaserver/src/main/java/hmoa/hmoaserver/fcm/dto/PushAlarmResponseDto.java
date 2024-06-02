7b0f6675907dee85a333ee9ae374970d054839c2
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2024-06-02 17:25:43 +0900

    [Feat] 푸쉬 알림 리스폰스 dto 구현 #89

--- hmoaserver/src/main/java/hmoa/hmoaserver/fcm/dto/PushAlarmResponseDto.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/fcm/dto/PushAlarmResponseDto.java
@@ -4,23 +4,21 @@ import hmoa.hmoaserver.common.DateUtils;
 import hmoa.hmoaserver.fcm.domain.PushAlarm;
 import lombok.Data;
 
-//@Data
-//public class PushAlarmResponseDto {
-//    private long id;
-//    private String category;
-//    private String target;
-//    private String content;
-//    private String createdAt;
-//    private long parentId;
-//    private boolean isRead;
-//
-//    public PushAlarmResponseDto(PushAlarm alarm, String category) {
-//        this.id = alarm.getId();
-//        this.target = alarm.getAlarmCategory().name();
-//        this.category = category;
-//        this.content = alarm.getContent();
-//        this.createdAt = DateUtils.extractAlarmDate(alarm.getCreatedAt());
-//        this.parentId = alarm.getParentId();
-//        this.isRead = alarm.isRead();
-//    }
-//}
+@Data
+public class PushAlarmResponseDto {
+    private long id;
+    private String title;
+    private String content;
+    private String deeplink;
+    private String createdAt;
+    private boolean isRead;
+
+    public PushAlarmResponseDto(PushAlarm alarm, String category) {
+        this.id = alarm.getId();
+        this.title = alarm.getTitle();
+        this.content = alarm.getContent();
+        this.deeplink = alarm.getDeeplink();
+        this.createdAt = DateUtils.extractAlarmDate(alarm.getCreatedAt());
+        this.isRead = alarm.isRead();
+    }
+}
