87b48366ed8d49de3cba3d6a14741f5a64d4aac8
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2024-07-03 21:12:33 +0900

    [Refactor] 추천 받은 향료 DTO에 향료 사진 추가 #93

--- hmoaserver/src/main/java/hmoa/hmoaserver/note/domain/Note.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/note/domain/Note.java
@@ -1,9 +1,12 @@
 package hmoa.hmoaserver.note.domain;
 
 import hmoa.hmoaserver.common.BaseEntity;
+import hmoa.hmoaserver.photo.domain.NotePhoto;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -23,12 +26,25 @@ public class Note extends BaseEntity {
     private String content;
     private boolean isDeleted = false;
 
+    @OneToMany(mappedBy = "note", cascade = CascadeType.ALL , orphanRemoval = true)
+    private List<NotePhoto> notePhotos = new ArrayList<>();
+
     public void updateContent(String content) {
         this.content = content;
     }
 
     public void delete() {
         isDeleted = true;
+    }
+
+    public NotePhoto getNotePhoto() {
+        int size = this.notePhotos.size();
+
+        if (size == 0) {
+            return null;
+        }
+
+        return notePhotos.get(notePhotos.size() - 1);
     }
 
 }
--- hmoaserver/src/main/java/hmoa/hmoaserver/note/dto/NoteSimpleResponseDto.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/note/dto/NoteSimpleResponseDto.java
@@ -5,11 +5,15 @@ import lombok.Data;
 
 @Data
 public class NoteSimpleResponseDto {
+    private Long noteId;
     private String noteName;
     private String content;
+    private String notePhotoUrl;
 
     public NoteSimpleResponseDto(Note note) {
+        this.noteId = note.getId();
         this.noteName = note.getTitle();
         this.content = note.getContent();
+        this.notePhotoUrl = note.getNotePhoto().getPhotoUrl();
     }
 }
