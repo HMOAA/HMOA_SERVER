f594f7a2927512ab9a1fe7ef60cc03211e936ec9
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2024-07-04 15:01:21 +0900

    [Feat] 세부 향료 저장 서비스 로직 구현 #105

---
+++ hmoaserver/src/main/java/hmoa/hmoaserver/note/service/DetailNoteService.java
@@ -0,0 +1,25 @@
+package hmoa.hmoaserver.note.service;
+
+import hmoa.hmoaserver.exception.Code;
+import hmoa.hmoaserver.exception.CustomException;
+import hmoa.hmoaserver.note.domain.DetailNote;
+import hmoa.hmoaserver.note.repository.DetailNoteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class DetailNoteService {
+
+    private final DetailNoteRepository detailNoteRepository;
+
+    public DetailNote save(DetailNote detailNote) {
+        try {
+            return detailNoteRepository.save(detailNote);
+        } catch (RuntimeException e) {
+            throw new CustomException(e, Code.SERVER_ERROR);
+        }
+    }
+}
