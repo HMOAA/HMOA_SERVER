98eb94fcdd6fb7d9297031f26e0d59df5492e6f3
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-07-13 15:35:48 +0900

    [Refacor] 노트 상품 페이지 dto 이미지 추가 #109

--- hmoaserver/src/main/java/hmoa/hmoaserver/hshop/dto/NoteProductResponseDto.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/dto/NoteProductResponseDto.java
@@ -12,12 +12,14 @@ public class NoteProductResponseDto {
     private Long productId;
     private String productName;
     private String productDetails;
+    private String productPhotoUrl;
     private Boolean isRecommended;
 
     public NoteProductResponseDto(NoteProduct noteProduct, boolean isRecommended) {
         this.productId = noteProduct.getId();
         this.productName = noteProduct.getNote().getTitle();
         this.productDetails = getDetails(noteProduct.getNote().getNoteDetailNotes());
+        this.productPhotoUrl = noteProduct.getNote().getNotePhoto().getPhotoUrl();
         this.isRecommended = isRecommended;
     }
 
