552a65b371d7848782c7c4847f73427a9fd8e034
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-07-22 14:40:13 +0900

    [Fix] jwt 에러 메세지 정상적으로 반환하도록 수정

--- hmoaserver/src/main/java/hmoa/hmoaserver/exception/CustomAuthenticationEntryPoint.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/exception/CustomAuthenticationEntryPoint.java
@@ -1,12 +1,11 @@
 package hmoa.hmoaserver.exception;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonObject;
 import hmoa.hmoaserver.oauth.jwt.service.JwtResultType;
 import hmoa.hmoaserver.oauth.jwt.service.JwtService;
 import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
@@ -17,32 +16,28 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStream;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    private static final String ERROR_CODE = "401";
 
-    @Value("${jwt.secret}")
-    private String secretKey;
+    private final JwtService jwtService;
 
+    private static final int ERROR_CODE = 401;
+    private static final int EMPTY_CODE = 404;
+    private static final String CODE_MESSAGE = "code";
+    private static final String MESSAGE_MESSAGE = "message";
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        JwtResultType errorType = isTokenValid(request.getHeader("X-AUTH-TOKEN"));
-        response.setContentType("application/json;charset=utf-8");
-        response.setStatus(401);
-        JsonObject json = new JsonObject();
-        json.addProperty("code", ERROR_CODE);
 
-        if (errorType.equals(JwtResultType.EXPIRED_JWT)) {
-            json.addProperty("message", Code.EXPIRED_TOKEN.getMessage());
-        } else if (errorType.equals(JwtResultType.INVALID_JWT)) {
-            json.addProperty("message", Code.WRONG_TYPE_TOKEN.getMessage());
-        } else {
-            json.addProperty("message", Code.UNSUPPORTED_TOKEN.getMessage());
-        }
+        JwtResultType errorType = isTokenValid(request.getHeader("X-AUTH-TOKEN"));
+
+        response.setContentType("application/json;charset=utf-8");
+
+        JsonObject json = getJsonObject(errorType);
+        response.setStatus(json.get(CODE_MESSAGE).getAsInt());
 
         log.info("{}", json.toString());
 
@@ -50,15 +45,30 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     }
 
     private JwtResultType isTokenValid(String token) {
-        try {
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(secretKey);
-            return JwtResultType.VALID_JWT;
-        }catch (ExpiredJwtException e) {
-            log.info("만료된 jwt 토큰");
-            return JwtResultType.EXPIRED_JWT;
-        }catch (Exception e) {
-            log.info("잘못된 jwt 토큰");
-            return JwtResultType.INVALID_JWT;
+        if (token == null || token.isEmpty()) {
+            return JwtResultType.EMPTY_JWT;
         }
+
+        return jwtService.isTokenValid(token);
+    }
+
+    private static JsonObject getJsonObject(JwtResultType type) {
+        JsonObject json = new JsonObject();
+        if (type.equals(JwtResultType.EMPTY_JWT)) {
+            json.addProperty(CODE_MESSAGE, EMPTY_CODE);
+            json.addProperty(MESSAGE_MESSAGE, Code.UNKNOWN_ERROR.getMessage());
+            return json;
+        }
+
+        json.addProperty(CODE_MESSAGE, ERROR_CODE);
+        if (type.equals(JwtResultType.EXPIRED_JWT)) {
+            json.addProperty(MESSAGE_MESSAGE, Code.EXPIRED_TOKEN.getMessage());
+        } else if (type.equals(JwtResultType.INVALID_JWT)) {
+            json.addProperty(MESSAGE_MESSAGE, Code.WRONG_TYPE_TOKEN.getMessage());
+        } else {
+            json.addProperty(MESSAGE_MESSAGE, Code.UNSUPPORTED_TOKEN.getMessage());
+        }
+
+        return json;
     }
 }
--- hmoaserver/src/main/java/hmoa/hmoaserver/oauth/jwt/service/JwtResultType.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/oauth/jwt/service/JwtResultType.java
@@ -1,5 +1,5 @@
 package hmoa.hmoaserver.oauth.jwt.service;
 
 public enum JwtResultType {
-    VALID_JWT, EXPIRED_JWT, INVALID_JWT
+    VALID_JWT, EXPIRED_JWT, INVALID_JWT, EMPTY_JWT
 }
