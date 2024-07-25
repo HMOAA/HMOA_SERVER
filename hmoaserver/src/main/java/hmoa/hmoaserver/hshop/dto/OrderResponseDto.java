093f1af86a78d31dc50bfc36f0c49d5aa436e24b
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-07-25 13:57:00 +0900

    [Feat] 주문 정보 창 가져오는 dto 구현 #109

---
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/dto/OrderResponseDto.java
@@ -0,0 +1,33 @@
+package hmoa.hmoaserver.hshop.dto;
+
+import hmoa.hmoaserver.hshop.domain.OrderEntity;
+import hmoa.hmoaserver.hshop.domain.OrderStatus;
+import hmoa.hmoaserver.member.dto.MemberAddressResponseDto;
+import hmoa.hmoaserver.member.dto.MemberInfoResponseDto;
+import lombok.Data;
+
+@Data
+public class OrderResponseDto {
+
+    private Long orderId;
+    private OrderStatus orderStatus;
+    private MemberInfoResponseDto memberInfo;
+    private MemberAddressResponseDto memberAddress;
+    private NoteProductsResponseDto noteProducts;
+    private int paymentAmount;
+    private int shippingAmount;
+    private int totalAmount;
+
+    public OrderResponseDto(OrderEntity orderEntity, MemberInfoResponseDto memberInfo,
+                            MemberAddressResponseDto memberAddressResponseDto, NoteProductsResponseDto noteProducts,
+                            int shippingAmount) {
+        this.orderId = orderEntity.getId();
+        this.orderStatus = orderEntity.getStatus();
+        this.memberInfo = memberInfo;
+        this.memberAddress = memberAddressResponseDto;
+        this.noteProducts = noteProducts;
+        this.paymentAmount = orderEntity.getTotalPrice();
+        this.shippingAmount = shippingAmount;
+        this.totalAmount = paymentAmount + shippingAmount;
+    }
+}
