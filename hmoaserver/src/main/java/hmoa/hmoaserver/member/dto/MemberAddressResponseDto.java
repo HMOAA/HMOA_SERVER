92080d3767efe68c1daf9ffcfd7498b6dfcb4da5
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-07-25 13:32:22 +0900

    [Refactor] 배송지 정보 가져오는 dto 구현 #109

---
+++ hmoaserver/src/main/java/hmoa/hmoaserver/member/dto/MemberAddressResponseDto.java
@@ -0,0 +1,29 @@
+package hmoa.hmoaserver.member.dto;
+
+import hmoa.hmoaserver.member.domain.MemberAddress;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberAddressResponseDto {
+
+    private String name;
+    private String addressName;
+    private String phoneNumber;
+    private String zipCode;
+    private String streetAddress;
+    private String detailAddress;
+    private String request;
+
+    public MemberAddressResponseDto(MemberAddress memberAddress) {
+        this.name = memberAddress.getName();
+        this.addressName = memberAddress.getAddressName();
+        this.phoneNumber = memberAddress.getPhoneNumber();
+        this.zipCode = memberAddress.getZipCode();
+        this.streetAddress = memberAddress.getStreetAddress();
+        this.detailAddress = memberAddress.getDetailAddress();
+        this.request = memberAddress.getRequest();
+    }
+}
