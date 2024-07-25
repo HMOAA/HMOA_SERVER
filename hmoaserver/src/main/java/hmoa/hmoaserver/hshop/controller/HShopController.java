a19dceca76cfc49fdbba91c80454f06bd5af7611
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-07-25 13:57:34 +0900

    [Feat] 주문 요청 창 가져오는 api 구현 #109

--- hmoaserver/src/main/java/hmoa/hmoaserver/hshop/controller/HShopController.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/controller/HShopController.java
@@ -2,15 +2,16 @@ package hmoa.hmoaserver.hshop.controller;
 
 import hmoa.hmoaserver.common.ResultDto;
 import hmoa.hmoaserver.hshop.domain.NoteProduct;
-import hmoa.hmoaserver.hshop.dto.NoteProductDetailResponseDto;
-import hmoa.hmoaserver.hshop.dto.NoteProductResponseDto;
-import hmoa.hmoaserver.hshop.dto.NoteProductSaveRequestDto;
-import hmoa.hmoaserver.hshop.dto.NoteProductSelectRequestDto;
+import hmoa.hmoaserver.hshop.domain.OrderEntity;
+import hmoa.hmoaserver.hshop.dto.*;
 import hmoa.hmoaserver.hshop.service.NoteProductService;
+import hmoa.hmoaserver.hshop.service.OrderService;
 import hmoa.hmoaserver.member.domain.Member;
+import hmoa.hmoaserver.member.domain.MemberAddress;
+import hmoa.hmoaserver.member.dto.MemberAddressResponseDto;
+import hmoa.hmoaserver.member.dto.MemberInfoResponseDto;
 import hmoa.hmoaserver.member.service.MemberService;
 import hmoa.hmoaserver.note.domain.Note;
-import hmoa.hmoaserver.note.dto.NoteWithDetailNotesDto;
 import hmoa.hmoaserver.note.service.NoteService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -29,9 +30,12 @@ import java.util.List;
 @RequestMapping("/shop")
 public class HShopController {
 
+    private static final int SHIPPING_FEE = 3000;
+
     private final NoteProductService noteProductService;
     private final NoteService noteService;
     private final MemberService memberService;
+    private final OrderService orderService;
 
     @ApiOperation("상품 등록")
     @PostMapping("/save")
@@ -53,7 +57,6 @@ public class HShopController {
 
         List<NoteProduct> noteProducts = noteProductService.getAllNoteProducts();
         List<String> recommendNotes = member.getNoteRecommend().getRecommendNotes();
-
         List<NoteProductResponseDto> result = new ArrayList<>();
 
         for (NoteProduct noteProduct : noteProducts) {
@@ -67,18 +70,34 @@ public class HShopController {
     @ApiOperation(value = "구매할 향료 보내기", notes = "상품 조회 시 받은 Product Id 보내주시면 됩니다")
     @PostMapping("/note/select")
     public ResponseEntity<ResultDto<Object>> selectNoteProduct(@RequestHeader("X-AUTH-TOKEN") String token, @RequestBody NoteProductSelectRequestDto dto) {
+        return ResponseEntity.ok(ResultDto.builder().data(getNoteProductDetails(dto)).build());
+    }
+
+    @ApiOperation(value = "주문 요청", notes = "선택한 향료 확인 후 결제 페이지로 넘어가는 API")
+    @PostMapping("note/order")
+    public ResponseEntity<OrderResponseDto> orderNote(@RequestHeader("X-AUTH-TOKEN") String token, @RequestBody NoteProductSelectRequestDto dto) {
 
         Member member = memberService.findByMember(token);
 
-        List<NoteProductDetailResponseDto> result = new ArrayList<>();
+        NoteProductsResponseDto noteProducts = getNoteProductDetails(dto);
+        OrderEntity order = orderService.firstOrderSave(member, dto.getProductIds(), noteProducts.getTotalPrice());
+        MemberAddressResponseDto memberAddress = new MemberAddressResponseDto(memberService.getMemberAddress(member));
+        MemberInfoResponseDto memberInfo = new MemberInfoResponseDto(memberService.getMemberInfo(member));
 
-        for (Long productId : dto.getProductIds()) {
-            NoteProduct noteProduct = noteProductService.getNoteProduct(productId);
-            NoteWithDetailNotesDto noteDetail = noteService.getDetailNotes(noteProduct.getNote().getId());
-            result.add(new NoteProductDetailResponseDto(productId, noteDetail));
-        }
-
-        return ResponseEntity.ok(ResultDto.builder().data(result).build());
+        return ResponseEntity.ok(new OrderResponseDto(order, memberInfo, memberAddress, noteProducts, SHIPPING_FEE));
     }
 
+    private NoteProductsResponseDto getNoteProductDetails(NoteProductSelectRequestDto dto) {
+
+        int totalPrice = 0;
+        List<NoteProductDetailResponseDto> noteProducts = new ArrayList<>();
+
+        for (Long productId : dto.getProductIds()) {
+            NoteProductDetailResponseDto noteProductDetailResponseDto = noteProductService.getNoteProductDetail(productId);
+            noteProducts.add(noteProductDetailResponseDto);
+            totalPrice += noteProductDetailResponseDto.getPrice();
+        }
+
+        return new NoteProductsResponseDto(totalPrice, noteProducts);
+    }
 }
