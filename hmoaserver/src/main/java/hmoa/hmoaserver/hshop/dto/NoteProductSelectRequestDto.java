2ef4020bc82455aefc023abe297fb7522758a10c
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-07-26 14:29:14 +0900

    [Feat] dto To CartEntity 구현 #109

--- hmoaserver/src/main/java/hmoa/hmoaserver/hshop/dto/NoteProductSelectRequestDto.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/dto/NoteProductSelectRequestDto.java
@@ -1,5 +1,6 @@
 package hmoa.hmoaserver.hshop.dto;
 
+import hmoa.hmoaserver.hshop.domain.Cart;
 import lombok.Data;
 
 import java.util.List;
@@ -8,4 +9,12 @@ import java.util.List;
 public class NoteProductSelectRequestDto {
 
     private List<Long> productIds;
+
+    public Cart toCartEntity(Long memberId, int totalPrice) {
+        return Cart.builder()
+                .memberId(memberId)
+                .totalPrice(totalPrice)
+                .productIds(productIds)
+                .build();
+    }
 }
