a9e5e396391697c5772a683cc89b5ae4962b5dd0
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-08-10 13:13:31 +0900

    [feat] 주문자 정보 조회 JPA 구현 #109

--- hmoaserver/src/main/java/hmoa/hmoaserver/member/repository/MemberInfoRepository.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/member/repository/MemberInfoRepository.java
@@ -3,5 +3,9 @@ package hmoa.hmoaserver.member.repository;
 import hmoa.hmoaserver.member.domain.MemberInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberInfoRepository extends JpaRepository<MemberInfo, Integer> {
+
+    Optional<MemberInfo> findByMemberId(Long memberId);
 }
