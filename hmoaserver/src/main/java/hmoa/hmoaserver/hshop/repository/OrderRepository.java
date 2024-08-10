a4197a2e8a1c78db45cc8adcf2f1c0a65359c7fa
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-08-10 14:23:06 +0900

    [feat] order 조회 JPA 구현 #109

--- hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/OrderRepository.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/OrderRepository.java
@@ -3,5 +3,9 @@ package hmoa.hmoaserver.hshop.repository;
 import hmoa.hmoaserver.hshop.domain.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<OrderEntity, Integer> {
+
+    Optional<OrderEntity> findById(Long id);
 }
