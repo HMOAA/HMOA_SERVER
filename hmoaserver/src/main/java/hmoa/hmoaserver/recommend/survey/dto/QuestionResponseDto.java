f5d72388829eaf8413ee2f5ef8270b00a422c31f
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-08-11 14:50:27 +0900

    [Refactor] 단수 응답, 복수 응답 여부 dto에 추가 #109

--- hmoaserver/src/main/java/hmoa/hmoaserver/recommend/survey/dto/QuestionResponseDto.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/recommend/survey/dto/QuestionResponseDto.java
@@ -1,6 +1,7 @@
 package hmoa.hmoaserver.recommend.survey.dto;
 
 import hmoa.hmoaserver.recommend.survey.domain.Question;
+import hmoa.hmoaserver.recommend.survey.domain.QuestionType;
 import lombok.Data;
 
 import java.util.List;
@@ -10,11 +11,13 @@ import java.util.stream.Collectors;
 public class QuestionResponseDto {
     private Long questionId;
     private String content;
+    private QuestionType questionType;
     private List<AnswerResponseDto> answers;
 
     public QuestionResponseDto(Question question) {
         this.questionId = question.getId();
         this.content = question.getContent();
+        this.questionType = question.getQuestionType();
         this.answers = question.getAnswers().stream().map(AnswerResponseDto::new).collect(Collectors.toList());
     }
 }
--- hmoaserver/src/main/java/hmoa/hmoaserver/recommend/survey/dto/QuestionSaveRequestDto.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/recommend/survey/dto/QuestionSaveRequestDto.java
@@ -1,16 +1,19 @@
 package hmoa.hmoaserver.recommend.survey.dto;
 
 import hmoa.hmoaserver.recommend.survey.domain.Question;
+import hmoa.hmoaserver.recommend.survey.domain.QuestionType;
 import hmoa.hmoaserver.recommend.survey.domain.Survey;
 import lombok.Data;
 
 @Data
 public class QuestionSaveRequestDto {
     private String content;
+    private QuestionType questionType;
 
     public Question toEntity(Survey survey) {
         return Question.builder()
                 .content(content)
+                .questionType(questionType)
                 .survey(survey)
                 .build();
     }
