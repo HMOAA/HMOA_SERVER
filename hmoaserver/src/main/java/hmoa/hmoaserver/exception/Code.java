b1d6ce396fb1064c14381d0d29c0243cf3d718ec
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-08-24 15:19:34 +0900

    [feat] Bootpay 에러코드 추가 #109

--- hmoaserver/src/main/java/hmoa/hmoaserver/exception/Code.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/exception/Code.java
@@ -15,6 +15,7 @@ public enum Code {
     BOARD_NOT_FOUND(BAD_REQUEST, "게시글을 찾을 수 없습니다."),
     ACCESS_DENIED(BAD_REQUEST, "권한이 없습니다."),
     APPLE_DECODE_ERROR(BAD_REQUEST, "애플 소셜 로그인 호출 오류"),
+    BOOTPAY_ERROR(BAD_REQUEST, "부트페이 결제 에러"),
 
     /**
      * 401 UNAUTHORIZED
