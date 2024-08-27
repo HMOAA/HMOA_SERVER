509cf2dbf779bd349c228e24ffaf17359f189bce
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-08-27 17:34:21 +0900

    [feat] 무중단 배포 sh 파일 구현 #121

---
+++ hmoaserver/deploy.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+EXIST_GREEN=$(docker ps | grep green)
+
+if [ -z "$EXIST_GREEN"]; then
+  echo "BLUE -> GREEN"
+  echo "블루에서 그린으로 변경합니다."
+  docker-compose pull green
+  docker-compose up -d green
+
+  while [ 1 = 1 ]; do
+  echo "그린 체크"
+  sleep 3
+
+  REQUEST=$(curl http://127.0.0.1:8080)
+    if [ -n "$REQUEST"]; then
+      echo "정상적으로 그린이 작동합니다"
+      break ;
+    fi
+  done
+
+  echo "nginx를 리로드 합니다."
+  sudo cp /etc/nginx.green.conf /etc/nginx/nginx.conf
+  sudo nginx -s rel
+
+  echo "블루를 종료합니다."
+  docker-compose stop blue
+
+else
+  echo "그린 -> 블루"
+  docker-compose pull blue
+  docker-compose up -d blue
+
+  while [ 1 = 1 ]; do
+  echo "블루 체크"
+  sleep 3
+
+  REQUEST=$(curl http://127.0.0.1:8081)
+    if [ -n "$REQUEST"]; then
+      echo "정상적으로 블루가 작동합니다"
+      break ;
+    fi
+  done
+
+  echo "nginx를 리로드 합니다."
+  sudo cp /etc/nginx.blue.conf /etc/nginx/nginx.conf
+  sudo nginx -s rel
+
+  echo "그린을 종료합니다."
+  docker-compose stop green
+
+sudo docker image prune -f
