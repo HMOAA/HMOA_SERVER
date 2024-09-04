61a283a3df2adcabd552a22e009278ee3534c288
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-09-04 16:39:32 +0900

    [feat] 복수 응답 여부 추가 #119

--- hmoaserver/src/main/java/hmoa/hmoaserver/recommend/survey/dto/PerfumeQuestionResponseDto.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/recommend/survey/dto/PerfumeQuestionResponseDto.java
@@ -1,22 +1,25 @@
 package hmoa.hmoaserver.recommend.survey.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
-@NoArgsConstructor
 @Getter
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PerfumeQuestionResponseDto {
 
     private String content;
     private List<PerfumeAnswerResponseDto> answer;
+    @Getter(AccessLevel.NONE)
+    private boolean isMultipleChoice;
+
+    public boolean getIsMultipleChoice() {
+        return isMultipleChoice;
+    }
 
     public PerfumeQuestionResponseDto(final String content, final List<PerfumeAnswerResponseDto> answer) {
         this.content = content;
         this.answer = answer;
+        this.isMultipleChoice = true;
     }
 }
