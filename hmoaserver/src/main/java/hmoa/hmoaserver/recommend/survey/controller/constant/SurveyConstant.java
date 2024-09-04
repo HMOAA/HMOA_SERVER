de543ea2b5c095ab93a57d0ae939759b78694a8e
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-09-04 16:24:58 +0900

    [feat] Survey에서 사용될 상수 정의 #119

---
+++ hmoaserver/src/main/java/hmoa/hmoaserver/recommend/survey/controller/constant/SurveyConstant.java
@@ -0,0 +1,7 @@
+package hmoa.hmoaserver.recommend.survey.controller.constant;
+
+public class SurveyConstant {
+
+    public static final String PERFUME_QUESTION = "시향 후 마음에 드는 향료를 골라주세요";
+    public static final Long QUESTION_ID = 16L;
+}
