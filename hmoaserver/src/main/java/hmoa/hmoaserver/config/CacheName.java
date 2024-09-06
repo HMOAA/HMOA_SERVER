1993dfe244e8ddaf08e9e8f444ded0b8de06c9dc
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-09-06 19:23:30 +0900

    feat: cacheName 상수로 구현 #129

---
+++ hmoaserver/src/main/java/hmoa/hmoaserver/config/CacheName.java
@@ -0,0 +1,8 @@
+package hmoa.hmoaserver.config;
+
+public class CacheName {
+    public static final String MAIN_PAGE = "MAIN_PAGE";
+    public static final String PERFUME_SURVEY =  "PERFUME_SURVEY";
+    public static final String NOTES = "NOTES";
+    public static final String QUESTION = "QUESTION";
+}
