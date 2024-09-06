0b8621af34a58dd67f3c69b4a7e0066bae17aefd
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-09-06 19:52:28 +0900

    feat: Survey Cache Type 추가 #130

--- hmoaserver/src/main/java/hmoa/hmoaserver/config/CacheType.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/config/CacheType.java
@@ -9,7 +9,9 @@ public enum CacheType {
     MAIN_PAGE(CacheName.MAIN_PAGE, 12, 1),
     NOTES(CacheName.NOTES, 12, 100),
     QUESTION(CacheName.QUESTION, 12, 30),
-    PERFUME_SURVEY(CacheName.PERFUME_SURVEY, 12, 1);
+    PERFUME_SURVEY(CacheName.PERFUME_SURVEY, 12, 1),
+    NOTE_SURVEY(CacheName.NOTE_SURVEY, 12, 1),
+    SURVEY(CacheName.SURVEY, 12, 10),;
 
     private final String cacheName;
     private final int expiredAfterWrite;
