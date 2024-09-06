fc9f192d87457714650f2d7747ce5e97fa4b5407
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-09-06 19:52:18 +0900

    feat: Survey Cache Name 추가 #130

--- hmoaserver/src/main/java/hmoa/hmoaserver/config/CacheName.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/config/CacheName.java
@@ -5,4 +5,6 @@ public class CacheName {
     public static final String PERFUME_SURVEY =  "PERFUME_SURVEY";
     public static final String NOTES = "NOTES";
     public static final String QUESTION = "QUESTION";
+    public static final String NOTE_SURVEY = "NOTE_SURVEY";
+    public static final String SURVEY = "SURVEY";
 }
