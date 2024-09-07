ec7675d3f901f039978066baf442a1ef3bb99c83
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-09-08 01:22:07 +0900

    feature: 메인 페이지 캐시 구현 #128

--- hmoaserver/src/main/java/hmoa/hmoaserver/config/CacheType.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/config/CacheType.java
@@ -6,7 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CacheType {
-    MAIN_PAGE(CacheName.MAIN_PAGE, 12, 1),
+    MAIN_PAGE(CacheName.MAIN_PAGE, 12, 5),
+    MAIN_PAGE_PERFUME(CacheName.MAIN_PAGE_PERFUME, 12, 30),
+    PERFUME(CacheName.PERFUME, 12, 500),
+    MAIN_PAGE_DTO(CacheName.MAIN_PAGE_DTO, 12, 5),
     NOTES(CacheName.NOTES, 12, 100),
     QUESTION(CacheName.QUESTION, 12, 30),
     PERFUME_SURVEY(CacheName.PERFUME_SURVEY, 12, 1),
