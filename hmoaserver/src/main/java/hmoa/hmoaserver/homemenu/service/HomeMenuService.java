0e7fa2ca2bea5225dbe74ae3709d4dadb8507944
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-09-08 01:23:15 +0900

    feature: 홈메뉴 가져올 때 perfumeHomeMenu 같이 가져오도록 구현 #128

--- hmoaserver/src/main/java/hmoa/hmoaserver/homemenu/service/HomeMenuService.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/homemenu/service/HomeMenuService.java
@@ -2,12 +2,9 @@ package hmoa.hmoaserver.homemenu.service;
 
 import hmoa.hmoaserver.homemenu.domain.HomeMenu;
 import hmoa.hmoaserver.homemenu.dto.HomeMenuSaveRequestDto;
-import hmoa.hmoaserver.perfume.domain.Perfume;
-
-import java.util.List;
 
 public interface HomeMenuService {
     HomeMenu save(HomeMenuSaveRequestDto dto);
     void modifyHomeMenu(HomeMenu homeMenu, String title);
-    HomeMenu findHomeMenuById(Long homeId);
+    HomeMenu getHomeMenu(Long homeId);
 }
