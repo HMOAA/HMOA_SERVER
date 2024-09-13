0aa0542e0f39bcebd41690ba39861f2488e54243
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-09-13 16:44:53 +0900

    refactor: 누락된 데이터 dto에 추가 #135

--- hmoaserver/src/main/java/hmoa/hmoaserver/recommend/survey/dto/PerfumeRecommendResponseDto.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/recommend/survey/dto/PerfumeRecommendResponseDto.java
@@ -15,12 +15,16 @@ public class PerfumeRecommendResponseDto {
     private Long perfumeId;
     private String perfumeName;
     private String perfumeEnglishName;
+    private String brandName;
+    private String perfumeImageUrl;
     private int price;
 
     public PerfumeRecommendResponseDto(final Perfume perfume) {
         this.perfumeId = perfume.getId();
         this.perfumeName = perfume.getKoreanName();
         this.perfumeEnglishName = perfume.getEnglishName();
+        this.brandName = perfume.getBrand().getBrandName();
+        this.perfumeImageUrl = perfume.getPerfumePhoto().getPhotoUrl();
         this.price = perfume.getPrice();
     }
 }
