3f91f5c75ff836925f62ab5c5eacce4e1bec03a2
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-09-14 16:33:48 +0900

    feat: memberId로 orderList 조회하기 #137

--- hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/OrderRepository.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/OrderRepository.java
@@ -3,9 +3,11 @@ package hmoa.hmoaserver.hshop.repository;
 import hmoa.hmoaserver.hshop.domain.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Integer> {
 
     Optional<OrderEntity> findById(Long id);
+    List<OrderEntity> findByMemberId(Long memberId);
 }
