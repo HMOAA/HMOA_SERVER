657775ef12a9e52eed9c180add536df278134db8
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-09-24 14:29:17 +0900

    feat: 배송 정보 등록 로직

---
+++ hmoaserver/src/main/java/hmoa/hmoaserver/admin/service/AdminFacade.java
@@ -0,0 +1,21 @@
+package hmoa.hmoaserver.admin.service;
+
+import hmoa.hmoaserver.admin.dto.OrderDeliverySaveRequestDto;
+import hmoa.hmoaserver.hshop.domain.OrderEntity;
+import hmoa.hmoaserver.hshop.domain.OrderStatus;
+import hmoa.hmoaserver.hshop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdminFacade {
+
+    private final OrderService orderService;
+
+    public void saveDeliveryInfo(OrderDeliverySaveRequestDto dto) {
+        OrderEntity order = orderService.findById(dto.getOrderId());
+        orderService.updateOrderStatus(order, OrderStatus.SHIPPING_PROGRESS);
+        orderService.updateDeliveryInfo(order, dto.getCourierCountry(), dto.getTrackingNumber());
+    }
+}
