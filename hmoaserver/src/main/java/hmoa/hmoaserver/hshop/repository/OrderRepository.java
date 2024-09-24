c564847317788bf82919cd23fbcb8fca418128ab
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-09-24 17:36:35 +0900

    feat: order 페이징해서 조회

--- hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/OrderRepository.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package hmoa.hmoaserver.hshop.repository;
 
 import hmoa.hmoaserver.hshop.domain.OrderEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +12,5 @@ public interface OrderRepository extends JpaRepository<OrderEntity, Integer> {
 
     Optional<OrderEntity> findById(Long id);
     List<OrderEntity> findByMemberId(Long memberId);
+    Page<OrderEntity> findByMemberId(Long memberId, Pageable pageable);
 }
