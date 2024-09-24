8d5b9e31e009a3ccfad3b2177bbe06ad0a5f77f7
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-09-24 13:53:26 +0900

    feat: 주문 취소 상태 추가

--- hmoaserver/src/main/java/hmoa/hmoaserver/hshop/domain/OrderStatus.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/domain/OrderStatus.java
@@ -11,6 +11,7 @@ public enum OrderStatus {
     PAY_CHECKING("결제 대기"),
     PAY_FAILED("결제 실패"),
     PAY_COMPLETE("결제 완료"),
+    PAY_CANCEL("주문 취소"),
     SHIPPING_WAITING("배송 준비 중"),
     SHIPPING_PROGRESS("배송 중"),
     SHIPPING_COMPLETE("배송 완료"),
