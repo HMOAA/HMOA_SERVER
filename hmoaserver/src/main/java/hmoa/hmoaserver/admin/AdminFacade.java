61c3a2d59ba3a0de490df459f50f270c21e08279
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-09-28 20:38:30 +0900

    feat: 배송 추적 로직

--- hmoaserver/src/main/java/hmoa/hmoaserver/admin/AdminFacade.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/admin/AdminFacade.java
@@ -1,7 +1,12 @@
 package hmoa.hmoaserver.admin;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hmoa.hmoaserver.admin.dto.*;
+import hmoa.hmoaserver.admin.dto.constant.TrackingQuery;
+import hmoa.hmoaserver.admin.dto.constant.TrackingStatus;
 import hmoa.hmoaserver.common.DateUtils;
+import hmoa.hmoaserver.exception.Code;
+import hmoa.hmoaserver.exception.CustomException;
 import hmoa.hmoaserver.hshop.domain.OrderEntity;
 import hmoa.hmoaserver.hshop.domain.OrderStatus;
 import hmoa.hmoaserver.hshop.service.OrderService;
@@ -29,10 +34,12 @@ public class AdminFacade {
 
     private final WebClient webClient;
     private final OrderService orderService;
+    private final ObjectMapper objectMapper;
 
-    public AdminFacade(WebClient.Builder webClientBuilder, OrderService orderService) {
+    public AdminFacade(WebClient.Builder webClientBuilder, OrderService orderService, ObjectMapper objectMapper) {
         this.webClient = webClientBuilder.baseUrl("https://apis.tracker.delivery").build();
         this.orderService = orderService;
+        this.objectMapper = objectMapper;
     }
 
     // 운송장 등록
@@ -45,7 +52,17 @@ public class AdminFacade {
     // 배송 상태 변화 받을 시 처리하는 로직
     public void checkTracking(TrackingCallbackRequestDto dto) {
         OrderEntity order = orderService.getByTrackingNumber(dto.getTrackingNumber());
-        log.info("check Tracking");
+        TrackingDeliveryRequestDto request = new TrackingDeliveryRequestDto(TrackingQuery.CHECK_QUERY.getQuery(), dto);
+        TrackingResponseDto response = webPost(request).map(this::mapToTrackingResponseDto).block();
+        String status = null;
+
+        if (response != null) {
+            status = response.getData().getTrack().getLastEvent().getStatus().getCode();
+        }
+
+        if (status != null && (status.equals(TrackingStatus.AVAILABLE_FOR_PICKUP.getValue()) || status.equals(TrackingStatus.DELIVERED.getValue()))) {
+            orderService.updateOrderStatus(order, OrderStatus.SHIPPING_COMPLETE);
+        }
     }
 
     public void updateOrderStatus(OrderStatusUpdateRequestDto dto) {
@@ -56,15 +73,33 @@ public class AdminFacade {
     public Mono<String> registerTrackWebhook(OrderDeliverySaveRequestDto dto) {
         String expirationTime = DateUtils.extractUTC(LocalDateTime.now().plusDays(5));
         WebhookInput webhookInput = new WebhookInput(dto.getTrackingNumber(), trackingCallbackUrl, expirationTime);
+        TrackingDeliveryRequestDto request = new TrackingDeliveryRequestDto(TrackingQuery.REGISTER_QUERY.getQuery(), Map.of("input", webhookInput));
+        return webPost(request);
+    }
 
-        RegisterTrackingRequestDto request = new RegisterTrackingRequestDto(Map.of("input", webhookInput));
+    /**
+     * 20x 이외의 코드 반환시 무한 요청한다고 한다..
+     * 오류가 나도 일단 200번 보내도록 에러 처리
+     */
+    private Mono<String> webPost(Object request) {
+        try {
+            return webClient.post()
+                    .uri("/graphql")
+                    .header(HttpHeaders.AUTHORIZATION, "TRACKQL-API-KEY " + trackingAccess + ":" + trackingSecret)
+                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .bodyValue(request)  // DTO를 body로 설정
+                    .retrieve()
+                    .bodyToMono(String.class);
+        } catch (Exception e) {
+            throw new CustomException(e, Code.TRAKING_FAILED);
+        }
+    }
 
-        return webClient.post()
-                .uri("/graphql")
-                .header(HttpHeaders.AUTHORIZATION, "TRACKQL-API-KEY " + trackingAccess + ":" + trackingSecret)
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .bodyValue(request)  // DTO를 body로 설정
-                .retrieve()
-                .bodyToMono(String.class);
+    private TrackingResponseDto mapToTrackingResponseDto(String response) {
+        try {
+            return objectMapper.readValue(response, TrackingResponseDto.class);
+        } catch (Exception e) {
+            throw new CustomException(e, Code.TRAKING_FAILED);
+        }
     }
 }
