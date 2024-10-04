62907f819de22b2bd904e72da9f143101ad4bf09
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-10-04 21:24:22 +0900

    feat: 좋아요 있는지 확인하는 JPA

--- hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/HbtiReviewHeartRepository.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/HbtiReviewHeartRepository.java
@@ -3,5 +3,9 @@ package hmoa.hmoaserver.hshop.repository;
 import hmoa.hmoaserver.hshop.domain.HbtiReviewHeart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HbtiReviewHeartRepository extends JpaRepository<HbtiReviewHeart, Long> {
+
+    Optional<HbtiReviewHeart> findByHbtiReviewIdAndMemberId(Long hbtiReviewId, Long memberId);
 }
