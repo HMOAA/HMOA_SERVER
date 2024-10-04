88aab540d12b4c6591dbc35396ab1d1d34c93714
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-10-04 20:54:10 +0900

    feat: hbti 리뷰 Jpa Repository

---
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/HbtiReviewRepository.java
@@ -0,0 +1,7 @@
+package hmoa.hmoaserver.hshop.repository;
+
+import hmoa.hmoaserver.hshop.domain.HbtiReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HbtiReviewRepository extends JpaRepository<HbtiReview, Long> {
+}
