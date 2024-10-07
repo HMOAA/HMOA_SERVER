1a1e97365c9f1e4f35bb0a2bb85e4ac37eba2765
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-10-07 20:30:36 +0900

    feat: hbti 리뷰 목록 조회

--- hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/HbtiReviewRepository.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/HbtiReviewRepository.java
@@ -1,7 +1,11 @@
 package hmoa.hmoaserver.hshop.repository;
 
 import hmoa.hmoaserver.hshop.domain.HbtiReview;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HbtiReviewRepository extends JpaRepository<HbtiReview, Long> {
+
+    Page<HbtiReview> findAllByOrderByHeartCountDescCreatedAtDesc(Pageable pageable);
 }
