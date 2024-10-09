7823d64aad7a7457865a31228840b33f3a2b9020
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-10-09 19:20:46 +0900

    feat: memberid, cursor로 Review 조회 JPA

--- hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/HbtiReviewRepository.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/HbtiReviewRepository.java
@@ -4,8 +4,16 @@ import hmoa.hmoaserver.hshop.domain.HbtiReview;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface HbtiReviewRepository extends JpaRepository<HbtiReview, Long> {
 
     Page<HbtiReview> findAllByOrderByHeartCountDescCreatedAtDesc(Pageable pageable);
+
+    @Query("SELECT hr " +
+            "FROM HbtiReview hr " +
+            "WHERE hr.memberId = ?1 AND " +
+            "hr.id < ?2 " +
+            "ORDER BY hr.createdAt DESC, hr.id Desc")
+    Page<HbtiReview> findAllByMemberIdOrderByCreatedAtDesc(Long memberId, Long cursor, Pageable pageable);
 }
