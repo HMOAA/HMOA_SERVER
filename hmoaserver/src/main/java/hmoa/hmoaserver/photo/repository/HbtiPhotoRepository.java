545c318845d3879e5d45de3b90db7e88693bb154
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-10-09 16:41:31 +0900

    feat: HBTI 사진 삭제 JPA

--- hmoaserver/src/main/java/hmoa/hmoaserver/photo/repository/HbtiPhotoRepository.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/photo/repository/HbtiPhotoRepository.java
@@ -2,6 +2,15 @@ package hmoa.hmoaserver.photo.repository;
 
 import hmoa.hmoaserver.photo.domain.HbtiPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface HbtiPhotoRepository extends JpaRepository<HbtiPhoto, Long> {
+
+    @Modifying
+    @Query("DELETE FROM HbtiPhoto p WHERE p.id IN :deleteIds")
+    void deleteByIds(@Param("deleteIds") List<Long> hbtiPhotos);
 }
