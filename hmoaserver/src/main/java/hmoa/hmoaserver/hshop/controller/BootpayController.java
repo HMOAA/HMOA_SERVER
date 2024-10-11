b4ebc6a6fb70e7843bcb3d04a2ab3c50e47f4315
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-10-11 23:50:48 +0900

    docs: 설명 추가

--- hmoaserver/src/main/java/hmoa/hmoaserver/hshop/controller/BootpayController.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/controller/BootpayController.java
@@ -3,6 +3,7 @@ package hmoa.hmoaserver.hshop.controller;
 import hmoa.hmoaserver.common.ResultDto;
 import hmoa.hmoaserver.exception.Code;
 import hmoa.hmoaserver.exception.CustomException;
+import hmoa.hmoaserver.fcm.service.FCMNotificationService;
 import hmoa.hmoaserver.hshop.domain.OrderEntity;
 import hmoa.hmoaserver.hshop.dto.BootpayCancelRequstDto;
 import hmoa.hmoaserver.hshop.dto.BootpayConfirmRequestDto;
@@ -11,6 +12,7 @@ import hmoa.hmoaserver.hshop.service.OrderService;
 import hmoa.hmoaserver.member.domain.Member;
 import hmoa.hmoaserver.member.service.MemberService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +32,7 @@ public class BootpayController {
     private final OrderService orderService;
     private final MemberService memberService;
 
+    @ApiOperation(value = "구매 승인하기")
     @PostMapping("/confirm")
     public ResponseEntity<ResultDto<Object>> confirm(@RequestHeader("X-AUTH-TOKEN") String token, @Valid @RequestBody BootpayConfirmRequestDto dto) {
         HashMap res = bootpayService.checkPayment(dto);
@@ -39,6 +42,7 @@ public class BootpayController {
                 .build());
     }
 
+    @ApiOperation(value = "주문 취소 (receiptId로, 사용 X)")
     @PostMapping("/cancel")
     public ResponseEntity<ResultDto<Object>> cancel(@RequestHeader("X-AUTH-TOKEN") String token, @Valid @RequestBody BootpayCancelRequstDto dto) {
         Member member = memberService.findByMember(token);
@@ -49,6 +53,7 @@ public class BootpayController {
                 .build());
     }
 
+    @ApiOperation(value = "주문 취소 (orderId로)")
     @DeleteMapping("/{orderId}/cancel")
     public ResponseEntity<ResultDto<Object>> cancel(@RequestHeader("X-AUTH-TOKEN") String token, @PathVariable("orderId") Long orderId) {
         Member member = memberService.findByMember(token);
