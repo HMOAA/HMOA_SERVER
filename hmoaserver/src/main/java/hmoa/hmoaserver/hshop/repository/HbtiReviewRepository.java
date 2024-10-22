b9f0101d5cca9a7483e6d90f7b9582b3aeb954fb
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-10-23 00:24:50 +0900

    feat: 리뷰 존재하는 지 확인하는 JPA

--- hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/HbtiReviewRepository.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/hshop/repository/HbtiReviewRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface HbtiReviewRepository extends JpaRepository<HbtiReview, Long> {
 
     Page<HbtiReview> findAllByOrderByHeartCountDescCreatedAtDesc(Pageable pageable);
@@ -16,4 +18,6 @@ public interface HbtiReviewRepository extends JpaRepository<HbtiReview, Long> {
             "hr.id < ?2 " +
             "ORDER BY hr.createdAt DESC, hr.id Desc")
     Page<HbtiReview> findAllByMemberIdOrderByCreatedAtDesc(Long memberId, Long cursor, Pageable pageable);
+
+    Optional<HbtiReview> findByOrderIdAndMemberId(Long orderId, Long memberId);
 }
