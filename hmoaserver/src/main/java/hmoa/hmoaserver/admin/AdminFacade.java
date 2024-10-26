6f79c7ddb22ed13ddf6bed71b3075c0b29f1358f
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-10-26 18:13:26 +0900

    feat: 운송 등록 로직 변경 및 배송 주문 조회 API

--- hmoaserver/src/main/java/hmoa/hmoaserver/admin/AdminFacade.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/admin/AdminFacade.java
@@ -10,6 +10,10 @@ import hmoa.hmoaserver.exception.CustomException;
 import hmoa.hmoaserver.hshop.domain.OrderEntity;
 import hmoa.hmoaserver.hshop.domain.OrderStatus;
 import hmoa.hmoaserver.hshop.service.OrderService;
+import hmoa.hmoaserver.member.dto.MemberAddressResponseDto;
+import hmoa.hmoaserver.member.dto.MemberInfoResponseDto;
+import hmoa.hmoaserver.member.service.MemberAddressService;
+import hmoa.hmoaserver.member.service.MemberInfoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -19,12 +23,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 @Component
 @Slf4j
 public class AdminFacade {
 
+    private final MemberAddressService memberAddressService;
+    private final MemberInfoService memberInfoService;
     @Value("${tracking.access}")
     private String trackingAccess;
     @Value("${tracking.secret}")
@@ -36,10 +43,12 @@ public class AdminFacade {
     private final OrderService orderService;
     private final ObjectMapper objectMapper;
 
-    public AdminFacade(WebClient.Builder webClientBuilder, OrderService orderService, ObjectMapper objectMapper) {
+    public AdminFacade(WebClient.Builder webClientBuilder, OrderService orderService, ObjectMapper objectMapper, MemberAddressService memberAddressService, MemberInfoService memberInfoService) {
         this.webClient = webClientBuilder.baseUrl("https://apis.tracker.delivery").build();
         this.orderService = orderService;
         this.objectMapper = objectMapper;
+        this.memberAddressService = memberAddressService;
+        this.memberInfoService = memberInfoService;
     }
 
     // 운송장 등록
@@ -65,6 +74,17 @@ public class AdminFacade {
         }
     }
 
+    // 배송 보내야 할 주문 받아오기
+    public List<OrderDeliveryListResponseDto> deliveryOrderList() {
+
+        List<OrderEntity> orders = orderService.getDeliveryOrders();
+        return orders.stream().map(order -> {
+            MemberAddressResponseDto address = new MemberAddressResponseDto(memberAddressService.findByMemberId(order.getMemberId()));
+            MemberInfoResponseDto info = new MemberInfoResponseDto(memberInfoService.findByMemberId(order.getMemberId()));
+            return new OrderDeliveryListResponseDto(order, address, info);
+        }).toList();
+    }
+
     public void updateOrderStatus(OrderStatusUpdateRequestDto dto) {
         OrderEntity order = orderService.findById(dto.getOrderId());
         orderService.updateOrderStatus(order, dto.getStatus());
@@ -74,7 +94,9 @@ public class AdminFacade {
         String expirationTime = DateUtils.extractUTC(LocalDateTime.now().plusDays(5));
         WebhookInput webhookInput = new WebhookInput(dto.getTrackingNumber(), trackingCallbackUrl, expirationTime);
         TrackingDeliveryRequestDto request = new TrackingDeliveryRequestDto(TrackingQuery.REGISTER_QUERY.getQuery(), Map.of("input", webhookInput));
-        return webPost(request);
+        return webPost(request)
+                .map(response -> "성공")
+                .onErrorReturn("실패");
     }
 
     /**
@@ -82,17 +104,16 @@ public class AdminFacade {
      * 오류가 나도 일단 200번 보내도록 에러 처리
      */
     private Mono<String> webPost(Object request) {
-        try {
-            return webClient.post()
-                    .uri("/graphql")
-                    .header(HttpHeaders.AUTHORIZATION, "TRACKQL-API-KEY " + trackingAccess + ":" + trackingSecret)
-                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                    .bodyValue(request)  // DTO를 body로 설정
-                    .retrieve()
-                    .bodyToMono(String.class);
-        } catch (Exception e) {
-            throw new CustomException(e, Code.TRAKING_FAILED);
-        }
+        return webClient.post()
+                .uri("/graphql")
+                .header(HttpHeaders.AUTHORIZATION, "TRACKQL-API-KEY " + trackingAccess + ":" + trackingSecret)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .bodyValue(request)  // DTO를 body로 설정
+                .retrieve()
+                .bodyToMono(String.class)
+                .doOnError(e -> {
+                    throw new CustomException(null, Code.TRAKING_FAILED);
+                });
     }
 
     private TrackingResponseDto mapToTrackingResponseDto(String response) {
