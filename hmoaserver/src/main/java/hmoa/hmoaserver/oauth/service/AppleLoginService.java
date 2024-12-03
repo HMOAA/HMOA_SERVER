a98b9632be297ff51c9d70d5f24a3e42389e3f1a
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-12-03 18:42:20 +0900

    chore: 확인용 로그 제거

--- hmoaserver/src/main/java/hmoa/hmoaserver/oauth/service/AppleLoginService.java
+++ hmoaserver/src/main/java/hmoa/hmoaserver/oauth/service/AppleLoginService.java
@@ -31,7 +31,6 @@ public class AppleLoginService {
     private final AppleProperties appleProperties;
 
     public AppleOAuth2UserInfo get(String token) {
-        log.info("{}", appleProperties.getTeamId());
         String idToken = appleAuthClient.getIdToken(
                 appleProperties.getClientId(),
                 generateClientSecret(),
